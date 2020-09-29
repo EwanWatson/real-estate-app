73f72f89352d5c75baf7a8d269265e08529e215c
EwanWatson <ehwatson94@gmail.com>
2020-09-29 10:54:51 +0100

    setting up urls and apps

--- btre/urls.py
+++ btre/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('listings/', incude('listings.urls')),
     path('admin/', admin.site.urls)
 ]
