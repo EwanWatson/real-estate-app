9091e544e47fa9584e8a8e4eb004abe771cedc64
EwanWatson <ehwatson94@gmail.com>
2020-10-09 12:46:08 +0100

    add mvp realtor

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from listings.models import Listing
+from realtors.models import Realtor
 # Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
@@ -9,4 +10,10 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvps = realtors.filter(is_mvp=True)
+    context = {
+        'realtors': realtors,
+        'mvps': mvps
+    }
+    return render(request, 'pages/about.html', context)
